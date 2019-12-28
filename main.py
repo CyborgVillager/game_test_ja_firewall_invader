f378821f6137143f8f4709d427fd911f1ec7dfba
CyborgVillager <almawijonathan@gmail.com>
2019-12-27 23:12:53 -0600

    v2

--- main.py
+++ main.py
@@ -16,12 +16,32 @@ player_Image = pygame.image.load('image/firewall_logo.png')
 playerX = 370
 playerY = 500
 
-def player():
+# Score
+
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+textX = 10
+testY = 10
+
+# Game Over
+over_font = pygame.font.Font('freesansbold.ttf', 64)
+
+def show_score(x, y):
+    score = font.render("Score : " + str(score_value), True, (255, 255, 255))
+    window_screen.blit(score, (x, y))
+
+def game_over_text():
+    over_text = over_font.render("GAME OVER", True, (255, 255, 255))
+    window_screen.blit(over_text, (200, 250))
+
+
+def player(x,y): # new values for x & y are able to be drawn on the screen
     # draw the player onto the screen
-    window_screen.blit(player_Image,(playerX,playerY))
+    window_screen.blit(player_Image,(x,y))
 
 
-#Game Loop
+# Game Loop
 running = True
 while running:
     # Has red,green, blue
@@ -32,8 +52,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    player()
+    playerY -= .1
+    player(playerX,playerY)
     pygame.display.update()
 
 
